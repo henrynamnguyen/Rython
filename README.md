07db67123a48084901c6dbcdf23856901260b106
Henry Nam Nguyen <67188030+henrynamnguyen@users.noreply.github.com>
2023-09-23 09:10:33 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,64 @@
+<h1 align="center">Rython</h1>
+
+My attempt at implementing a simple language using LLVM components in Rust.
+
+### Features:
+- Python-like syntax
+- Protoype declarations
+- Functions
+- Conditionals
+- Loops
+- Comments
+
+## Usage
+
+```bash
+# Run cobra program from file.
+cargo run <filename>
+
+# Run code interactively (parsing from stdin)
+cargo run
+```
+
+## Example
+
+```python
+# fib.ks: Prints the fibonacci sequence up to n.
+def fib(n)
+  if n < 3 then
+    1
+  else
+    fib(n-1) + fib(n-2);
+
+fib(10);
+```
+
+```bash
+$ cargo run fib.ks
+Parse fib.ks.
+[src/parser.rs:185] self.cur_tok() = Then
+Parse 'def'
+define double @fib(double %n) {
+block:
+  %fcmpult = fcmp ult double %n, 3.000000e+00 # check if n is less than 3 (if condition)
+  br i1 %fcmpult, label %block5, label %block2 # if n is less than 3, branch to block5, otherwise to block2
+
+block2:                                           ; preds = %block
+  %fsub = fadd double %n, -1.000000e+00 # subtract 1 from n
+  %call = call double @fib(double %fsub) # call function fib recursively
+  %fsub3 = fadd double %n, -2.000000e+00 # subtract 2 from n
+  %call4 = call double @fib(double %fsub3) # call function fib recursively
+  %fadd = fadd double %call, %call4 # add result of the two recursive calls
+  br label %block5
+
+block5:                                           ; preds = %block, %block2
+  %phi = phi double [ %fadd, %block2 ], [ 1.000000e+00, %block ] # if coming from block, return 1, otherwise return value from block2 (phi node, SSA)
+  ret double %phi
+}
+define double @__anon_expr() {
+block:
+  %call = call double @fib(double 1.000000e+01) # call function fib with n = 10
+  ret double %call # return value from function call
+}
+Evaluated to 55
+```
---
+++ src/lexer.rs
@@ -0,0 +1,118 @@
+use std::iter::Peekable;
+
+#[derive(Debug, PartialEq)]
+pub enum Token {
+    Def,
+    Extern,
+    Delimiter,
+    OpeningParenthesis,
+    ClosingParenthesis,
+    Comma,
+    Identifier(String),
+    Number(f64),
+    Operator(String),
+    Char(char),
+    If,
+    Then,
+    Else,
+    For,
+    In,
+}
+
+pub struct Lexer<I>
+    where I: Iterator<Item=char>
+{
+    input: Peekable<I>,
+    last_char: Option<char>,
+}
+
+impl<I> Lexer<I>
+    where I: Iterator<Item=char>,
+{
+    pub fn new(mut input: I) -> Lexer<I> {
+        let last_char = input.next();
+        Lexer {
+            input: input.peekable(),
+            last_char: last_char,
+        }
+    }
+
+    fn step(&mut self) -> Option<char> {
+        self.last_char = self.input.next();
+        self.last_char
+    }
+
+    pub fn gettok(&mut self) -> Token {
+        while matches!(self.last_char, Some(' ') | Some('\t') | Some('\n')) {
+            self.step();
+        }
+
+        let last_char = if let Some(c) = self.last_char {
+            c
+        } else {
+            return Token::Eof;
+        };
+
+        if last_char.is_ascii_alphabetic() {
+            let mut identifier = String::new();
+            identifier.push(last_char);
+            
+            while let Some(&c) = self.input.peek() {
+                if c.is_ascii_alphanumeric() {
+                    identifier.push(c);
+                    self.step();
+                } else {
+                    break;
+                }
+            }
+
+            match identifier.as_str() {
+                "def" => Token::Def,
+                "extern" => Token::Extern,
+                "if" => Token::If,
+                "then" => Token::Then,
+                "else" => Token::Else,
+                "for" => Token::For,
+                "in" => Token::In,
+                _ => Token::Identifier(identifier),
+            }
+
+        } else if last_char.is_ascii_digit() || last_char == '.' {
+            let mut number = String::new();
+            number.push(last_char);
+            while let Some(&c) = self.input.peek() {
+                if c.is_ascii_digit() || c == '.' {
+                    number.push(c);
+                    self.step();
+                } else {
+                    break;
+                }
+            }
+            Token::Number(number.parse().expect("Lexer: Invalid number"))
+
+        } else if last_char == '#' {
+            while let Some(&c) = self.input.peek() {
+                if c != '\n' {
+                    self.step();
+                } else {
+                    break;
+                }
+            }
+            self.gettok()
+
+        } else {
+            let mut operator = String::new();
+            operator.push(last_char);
+            while let Some(&c) = self.input.peek() {
+                if !c.is_ascii_alphanumeric() {
+                    operator.push(c);
+                    self.step();
+                } else {
+                    break;
+                }
+            }
+
+            Token::Operator(operator)
+        }
+    }
+}
